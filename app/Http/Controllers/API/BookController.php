7edfb8c2306ebef2da47260c7d1cb1b3fec884fd
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2022-08-08 10:51:34 +0700

    update book crud

--- app/Http/Controllers/API/BookController.php
+++ app/Http/Controllers/API/BookController.php
@@ -2,10 +2,150 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Helpers\DTO;
+use App\Models\Book;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BookResource;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class BookController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::all();
+
+        return DTO::ResponseDTO('Books List', null, $books, Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'desc' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return DTO::ResponseDTO($validator->errors(), null, null, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try {
+            $book = Book::create([
+                'title' => $request->title,
+                'desc' => $request->desc
+            ]);
+        } catch (\Throwable $th) {
+            return DTO::ResponseDTO('Failed Create Book', null, null, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return DTO::ResponseDTO('Book Created Successfully', null, $book, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $book = Book::find($id);
+        if (is_null($book)) {
+            return DTO::ResponseDTO('Book Not Found', null, null, Response::HTTP_NOT_FOUND);
+        }
+
+        return DTO::ResponseDTO('Show Book Successfully', null, $book, Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $book = Book::find($id);
+
+        if (is_null($book)) {
+            return DTO::ResponseDTO('Book Not Found', null, null, Response::HTTP_NOT_FOUND);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'desc' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return DTO::ResponseDTO($validator->errors(), null, null, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try {
+            $book->update([
+                'title' => $request->title,
+                'desc' => $request->desc
+            ]);
+        } catch (\Throwable $th) {
+            return DTO::ResponseDTO('Failed Update Book', null, null, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return DTO::ResponseDTO('Book Update Successfully', null, $book, Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $book = Book::find($id);
+
+        if (is_null($book)) {
+            return DTO::ResponseDTO('Book Not Found', null, null, Response::HTTP_NOT_FOUND);
+        }
+
+        try {
+            $book->delete();
+        } catch (\Throwable $th) {
+            return DTO::ResponseDTO('Delete Book Failed', null, null, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return DTO::ResponseDTO('Delete Book Successfully', null, null, Response::HTTP_OK);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProgramController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,12 @@ use App\Http\Controllers\API\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 //API route for login user
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('programs', [ProgramController::class, 'index']);
+Route::post('programs/store', [ProgramController::class, 'store']);
+Route::get('programs/show/{id}', [ProgramController::class, 'show']);
+Route::put('programs/update/{id}', [ProgramController::class, 'update']);
+Route::delete('programs/destroy/{id}', [ProgramController::class, 'destroy']);
 
 //Protecting Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
